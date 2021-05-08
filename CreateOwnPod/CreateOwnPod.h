40f09c809b2eaffb195122042d871d54cbbf4002
bahadirseyfi <bahadirsoftware@gmail.com>
2021-05-08 12:27:53 +0300

    Initial Commit

---
+++ CreateOwnPod/CreateOwnPod.h
@@ -0,0 +1,18 @@
+//
+//  CreateOwnPod.h
+//  CreateOwnPod
+//
+//  Created by bahadir on 8.05.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CreateOwnPod.
+FOUNDATION_EXPORT double CreateOwnPodVersionNumber;
+
+//! Project version string for CreateOwnPod.
+FOUNDATION_EXPORT const unsigned char CreateOwnPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CreateOwnPod/PublicHeader.h>
+
+
